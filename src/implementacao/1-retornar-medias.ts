7beada33bdbdba81fe92894101e272b34da048ae
Franciele Tadiello <franciele.p.t@gmail.com>
2025-07-07 11:42:34 -0300

    Calcular média de cada aluna

--- src/implementacao/1-retornar-medias.ts
+++ src/implementacao/1-retornar-medias.ts
@@ -1,3 +1,12 @@
-function retornarMedias(){
-    return null
+import { Alunas, turma } from "../../listaPreConstruida";
+
+function retornarMedias( alunas: Alunas) : number[] {
+    return alunas.map(aluna => {
+        const { p1, p2, p3} = aluna.prova;
+        const media = (p1+ p2+ p3)/3;
+        return (media);
+    })
+    
 }
+
+console.log(retornarMedias(turma));
