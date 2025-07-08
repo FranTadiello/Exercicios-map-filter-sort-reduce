9e98e5f1a542a18e6b53afe7f4f07f36b2153c26
Franciele Tadiello <franciele.p.t@gmail.com>
2025-07-08 14:22:50 -0300

    Retornando aluna com maior média

--- src/implementacao/5-retornar-maior-media.ts
+++ src/implementacao/5-retornar-maior-media.ts
@@ -1,3 +1,10 @@
-function retornarMaiorMedia() {
-    return null;
+import { Alunas, turma} from "../../listaPreConstruida";
+import { alunasComMedia } from "./1-retornar-medias";
+
+function retornarMaiorMedia(alunas : Alunas) : string {
+    const melhor = alunasComMedia(alunas) 
+        .reduce ((maior, atual) => (atual.media > maior.media ? atual : maior))
+    return melhor.nome;
 }
+
+console.log(retornarMaiorMedia(turma));
