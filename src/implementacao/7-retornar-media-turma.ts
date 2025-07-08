a4e5e64767ca49c317aad63e9b37aa6de0504f93
Franciele Tadiello <franciele.p.t@gmail.com>
2025-07-08 14:55:37 -0300

    Calculando a média da turma

--- src/implementacao/7-retornar-media-turma.ts
+++ src/implementacao/7-retornar-media-turma.ts
@@ -1,3 +1,11 @@
-function retornarMediaTurma() {
-    return null;
+import { Alunas, turma} from "../../listaPreConstruida";
+import { retornarMedias } from "./1-retornar-medias";
+
+function retornarMediaTurma( alunas : Alunas) : number {
+    const somaMedia = retornarMedias(alunas)
+        .reduce ((total, nota) => total + nota, 0);
+    const mediaTurma = somaMedia / alunas.length;
+    return parseFloat(mediaTurma.toFixed(1));
 }
+
+console.log(retornarMediaTurma(turma));
