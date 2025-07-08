128d692d23792d5a9bcd4a2a4c54e66b296d4cc5
Franciele Tadiello <franciele.p.t@gmail.com>
2025-07-08 13:49:28 -0300

    Criando lista da turma com boolean de aprovado

--- src/implementacao/4-retornar-objetos.ts
+++ src/implementacao/4-retornar-objetos.ts
@@ -1,3 +1,19 @@
-function retornarObjetos() {
-    return null;
+import { Alunas, turma} from "../../listaPreConstruida";
+import { retornarMedias } from "./1-retornar-medias";
+
+type ListaComMedia = { 
+          nome: String , 
+          media: Number, 
+          aprovada: Boolean 
+      }
+      
+function retornarObjetos( alunas: Alunas) : ListaComMedia[] {
+    const medias = retornarMedias(alunas);
+    return alunas.map ((aluna, index) => ({ 
+        nome: aluna.nome, 
+        media:medias[index],
+        aprovada: medias[index] >= 7 
+    }));
 }
+
+console.log(retornarObjetos(turma));
