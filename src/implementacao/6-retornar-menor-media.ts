df9e2b38f2b15e0317e3e8cd86e9ff2a36c386df
Franciele Tadiello <franciele.p.t@gmail.com>
2025-07-08 14:26:40 -0300

    Retornando aluna com menor média

--- src/implementacao/6-retornar-menor-media.ts
+++ src/implementacao/6-retornar-menor-media.ts
@@ -1,3 +1,10 @@
-function retornarMenorMedia() {
-    return null;
+import { Alunas, turma} from "../../listaPreConstruida";
+import { alunasComMedia } from "./1-retornar-medias";
+
+function retornarMenorMedia( alunas: Alunas) : string {
+    return alunasComMedia(alunas) 
+        .reduce ((menor, atual) => (atual.media < menor.media ? atual : menor))
+        .nome
 }
+
+console.log(retornarMenorMedia(turma));
