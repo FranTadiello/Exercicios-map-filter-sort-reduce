52638e404888d9338aa55efe468540d82656adf1
Franciele Tadiello <franciele.p.t@gmail.com>
2025-07-08 13:28:23 -0300

    Criando função para retornar alunas reprovadas

--- src/implementacao/3-retornar-reprovadas.ts
+++ src/implementacao/3-retornar-reprovadas.ts
@@ -1,3 +1,12 @@
-function retornarReprovadas() {
-    return null;
+import { Alunas, turma} from "../../listaPreConstruida";
+import { retornarMedias } from "./1-retornar-medias";
+
+function retornarReprovadas( alunas : Alunas) {
+    const medias = retornarMedias(alunas);
+    return alunas
+        .map ((aluna, index) => ({ nome: aluna.nome, media:medias[index] }))
+        .filter((aluna) => aluna.media < 7)
+        .map((reprovada) => reprovada.nome );
 }
+
+console.log(retornarReprovadas(turma));
